7daf2f91761c5eecbb87fbf0de675a9172ac0fcb
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-07-14 14:17:50 +0300

    finnish

--- shop-exam/src/store/store.ts
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import countryReducer from "./reducers/countryReducer";
-import populationReducer from "./reducers/populationReducer";
-
-export default configureStore({
-    reducer: { country: countryReducer, population: populationReducer },
-});
